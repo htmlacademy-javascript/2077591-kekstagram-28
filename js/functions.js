5e17c5678dc20b26abe5bdc456270eec6bcd4ed0
RomanBelyankin <105749531+RomanBelyankin@users.noreply.github.com>
2023-02-26 12:05:42 +0400

    2.28. Нужно больше функций (доработки)
    последнее задание не очень понял как делать если честно, подсмотрел на видеоразборе, сложное

--- js/functions.js
+++ js/functions.js
@@ -3,166 +3,112 @@
 имя_функции('проверяемая строка', 20); // Результат: true - строка проходит по длине
 имя_функции('проверяемая строка', 18); // Результат: true - строка проходит по длине
 имя_функции('проверяемая строка', 10); // Результат: false — строка не проходит
-
 */
 
-let a = "проверяемая строка";
-console.log (a.length);
-
-function string_length(a, len) {
-  return a.length <= len
+let string = ("проверяемая строка");
+function stringLength(a, len) {
+  return string.length <= len
 }
+const result1 = stringLength(string, 20);
+//console.log(result1);
+const result2 = stringLength(string, 18);
+//console.log(result2);
+const result3 = stringLength(string, 10);
+//console.log(result3);
 
-const result1 = string_length(a, 20);
-console.log (result1);
-const result2 = string_length(a, 18);
-console.log (result2);
-const result3 = string_length(a, 10);
-console.log (result3);
+//другие способы
 
+const checkLength = (string, length) => {
+  if (string.length <= length) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+const res = checkLength('проверяемая строка', 20);
+//console.log(res);
 
 
 /*
-имя_функции('топот'); // Результат: true - строка является палиндромом
-имя_функции('ДовОд'); // Результат: true - несмотря на разный регистр, тоже палиндром
-имя_функции('Кекс');  // Результат: false - это не палиндром
-*/
 
+// Строка является палиндромом
+имяФункции('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+имяФункции('ДовОд'); // true
+// Это не палиндром
+имяФункции('Кекс');  // false
+Если хотите усложнить задание, предусмотрите случай, когда в строке встречаются пробелы. Они не должны учитываться при проверке!
 
-/* ТОПОТ */
-
-
-function reverseString(str) {
-  // Шаг 1. Использую метод split(), чтобы получить массив из строки
-  var splitString = str.split("");
-
-  // Шаг 2. Использую метод reverse(), чтобы перевернуть элементы в полученном массиве
-  var reverseArray = splitString.reverse();
-
-  // Шаг 3. Использую метод join(), чтобы соединить элементы массива
-  var joinArray = reverseArray.join("");
-
-  // Шаг 4. Возвращаю перевернутую строку
-  return joinArray; //
-}
-
-let topot = 'топот';
-
-let topot_reverse = reverseString(topot);
-console.log (topot_reverse);
-
-function comparison1 (topot, topot_reverse) {
-  return topot===topot_reverse
-  };
-
-let result_one = comparison1 (topot, topot_reverse);
-console.log (result_one);
-
-
-
-
-
-/* ДовОд */
-
-function reverseString(str) {
-  // Шаг 1. Использую метод split(), чтобы получить массив из строки
-  var splitString = str.split("");
-
-  // Шаг 2. Использую метод reverse(), чтобы перевернуть элементы в полученном массиве
-  var reverseArray = splitString.reverse();
-
-  // Шаг 3. Использую метод join(), чтобы соединить элементы массива
-  var joinArray = reverseArray.join("");
-
-  // Шаг 4. Возвращаю перевернутую строку
-  return joinArray; //
-}
-
-let dovod = 'ДовОд';
-
-let dovod_reverse = reverseString(dovod);
-console.log (dovod);
-
-function comparison2 (dovod, dovod_reverse) {
-  return dovod.toLowerCase===dovod_reverse.toLowerCase
-  };
-
-let result_two = comparison2 (dovod, dovod_reverse);
-console.log (result_two);
-
-
-
-
-
-
-/* Кекс */
-
-function reverseString(str) {
-  // Шаг 1. Использую метод split(), чтобы получить массив из строки
-  var splitString = str.split("");
-
-  // Шаг 2. Использую метод reverse(), чтобы перевернуть элементы в полученном массиве
-  var reverseArray = splitString.reverse();
-
-  // Шаг 3. Использую метод join(), чтобы соединить элементы массива
-  var joinArray = reverseArray.join("");
-
-  // Шаг 4. Возвращаю перевернутую строку
-  return joinArray; //
-}
-
-let keks = 'кекс';
-
-let keks_reverse = reverseString(keks);
-console.log (keks);
-
-function comparison3 (keks, keks_reverse) {
-  return keks===keks_reverse
-  };
-
-const result_three = comparison3 (keks, keks_reverse);
-console.log (result_three);
-
-
-
-
-
-
-
-/*
-имя_функции('2023 год');            // Результат: число 2023
-имя_функции('ECMAScript 2022');     // Результат: число 2022
-имя_функции('1 кефир, 0.5 батона'); // Результат: число 105
-имя_функции('а я томат');           // Результат: NaN
-
-имя_функции(2023); // Результат: число 2023
-имя_функции(-1);   // Результат: число 1
-имя_функции(1.5);  // Результат: число 15
+// Это палиндром
+имяФункции('Лёша на полке клопа нашёл '); // true
 
 */
 
-console.log (parseInt('2023 год'));
-console.log (parseInt('ECMAScript 2022'));
-console.log (parseFloat('1 кефир, 0.5 батона'));
-console.log (parseInt('а я томат'));
 
+const isPolindrome = (string) => {
+  const tempString = string.toLocaleLowerCase().replaceAll(' ','');
+
+  let reversString = '';
+  for (let i = tempString - 1; i >= 0; i--) {
+    reversString += tempString.at(i);
+  }
+  //console.log(reversString);
+  return tempString === reversString;
+}
+isPolindrome ('топот');
 
 /*
+
+имяФункции('2023 год');            // 2023
+имяФункции('ECMAScript 2022');     // 2022
+имяФункции('1 кефир, 0.5 батона'); // 105
+имяФункции('агент 007');           // 7
+имяФункции('а я томат');           // NaN
+
+*/
+
+const extractNumber = (string) => {
+  let result = '';
+  for (let i=0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+  return parseInt(result, 10);
+}
+
+/*
+
+Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
+
+Эта функция нам пригодится для формирования адресов файлов. Примеры её использования:
 
 // Добавочный символ использован один раз
-имя_функции('1', 2, '0');      // Результат: строка '01'
+имяФункции('1', 2, '0');      // '01'
 
 // Добавочный символ использован три раза
-имя_функции('1', 4, '0');      // Результат: строка '0001'
+имяФункции('1', 4, '0');      // '0001'
 
 // Добавочные символы обрезаны с конца
-имя_функции('q', 4, 'werty');  // Результат: строка 'werq'
+имяФункции('q', 4, 'werty');  // 'werq'
 
 // Добавочные символы использованы полтора раза
-имя_функции('q', 4, 'we');     // Результат: строка 'wweq'
+имяФункции('q', 4, 'we');     // 'wweq'
 
 // Добавочные символы не использованы, исходная строка не изменена
-имя_функции('qwerty', 4, '0'); // Результат: строка 'qwerty'
+имяФункции('qwerty', 4, '0'); // 'qwerty'
+
 
 */
 
+const myPadStart = (string, minLenght, pad) => {
+  const actualPad = minLenght - string.length;
+
+  if (actualPad <= 0) {
+    return string;
+  }
+
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad
+    / pad.length) + string;
+}
